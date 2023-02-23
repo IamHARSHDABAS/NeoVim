99b8ea7283707f407ef38942642da01b7ab0be61
Harsh Dabas <iamharshdabas@gmail.com>
2023-02-23 15:40:13 +0530

    Rm remaps

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -3,12 +3,4 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Remaps
-vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
-
--- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
--- TODO
--- vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<Leader>e', ':NvimTreeFindFileToggle<CR>')
