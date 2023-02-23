9de97379d41f81b22ae1e48ca26fef969cbd019f
Harsh Dabas <iamharshdabas@gmail.com>
2023-02-23 15:37:29 +0530

    Cleaned

--- init.lua
+++ init.lua
@@ -1,2 +1,1 @@
--- require('core')
-require('user')
+require('core')
--- lua/core/config/lsp.lua
+++ lua/core/config/lsp.lua
@@ -1,8 +1,3 @@
 require("mason").setup{}
 
-require("mason-lspconfig").setup {
-	ensure_installed = { "jdtls", "sumneko_lua" },
-}
-
-require('lspconfig').jdtls.setup{}
-require('lspconfig').sumneko_lua.setup{}
+require("mason-lspconfig").setup()
