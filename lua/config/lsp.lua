9d343717e5dbb20bc1f9c4201f93cf1fb8b0e96d
Harsh Dabas <iamharshdabas@gmail.com>
2023-02-23 16:47:14 +0530

    Updated lsp

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,3 +1,17 @@
-require("mason").setup{}
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗"
+		}
+	}
+})
 
-require("mason-lspconfig").setup()
+require("mason-lspconfig").setup {
+	ensure_installed = { "jdtls", "lua_ls", "pyright" }
+}
+
+require("lspconfig").jdtls.setup {}
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").pyright.setup {}
