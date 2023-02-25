35fe34194af2b05d3b87ca186c6e00a930a77044
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-02-25 21:44:07 +0530

    Reworked

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,9 +1,3 @@
-require('core.plugin')
-require('core.remap')
-require('core.setting')
-require('config.comment')
-require('config.lsp')
-require('config.lualine')
-require('config.nvimtree')
-require('config.telescope')
-require('config.treesitter')
+require 'core.setting'
+require 'core.keymap'
+require 'core.plugin'
--- lua/core/remap.lua
+++
@@ -1,6 +0,0 @@
--- Changing <LEADER> to <SPACE>
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Remaps
-vim.keymap.set('n', '<Leader>e', ':NvimTreeFindFileToggle<CR>')
