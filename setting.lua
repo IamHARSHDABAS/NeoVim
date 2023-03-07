96e6ba970ed3c1c93423b9d17973d0a5a8cbb4cf
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-07 22:39:39 +0530

    Updated settings

---
+++ setting.lua
@@ -0,0 +1,25 @@
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+
+vim.opt.whichwrap = "bs<>[]hl"
+vim.opt.smartindent = true
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+vim.opt.termguicolors = true
+vim.opt.showmode = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = false
+
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
+vim.opt.updatetime = 50
