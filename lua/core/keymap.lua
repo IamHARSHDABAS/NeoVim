d04139601fdd65bb526be1095d422ee419a793a7
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-07 14:53:21 +0530

    Lazy to revert

---
+++ lua/core/keymap.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "D", "<C-d>zz")
+vim.keymap.set("n", "U", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
