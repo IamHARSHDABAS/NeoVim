197956860a9cb2fa31b5fee262300781ada776c3
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-07 16:55:17 +0530

    Cleanup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,5 +11,5 @@ lsp.nvim_workspace()
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true
+  virtual_text = true
 })
--- lua/core/plugin.lua
+++ lua/core/plugin.lua
@@ -1,7 +1,7 @@
 require('packer').startup(function(use)
-	-- Packer
-	use 'wbthomason/packer.nvim'
-	-- Lualine
+  -- Packer
+  use 'wbthomason/packer.nvim'
+  -- Lualine
   use 'nvim-lualine/lualine.nvim'
   -- Bufferline
   use 'akinsho/bufferline.nvim'
@@ -9,35 +9,35 @@ require('packer').startup(function(use)
   use 'lukas-reineke/indent-blankline.nvim'
   -- Telescope
   use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		-- or                          , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	-- Treesitter
-	use 'nvim-treesitter/nvim-treesitter'
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    -- or                          , branch = '0.1.x',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  -- Treesitter
+  use 'nvim-treesitter/nvim-treesitter'
   -- Flutter
   use 'akinsho/flutter-tools.nvim'
-	-- LSP zero
-	use {
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v1.x',
-		requires = {
-			-- LSP Support
-			{'neovim/nvim-lspconfig'},             -- Required
-			{'williamboman/mason.nvim'},           -- Optional
-			{'williamboman/mason-lspconfig.nvim'}, -- Optional
-			-- Autocompletion
-			{'hrsh7th/nvim-cmp'},         -- Required
-			{'hrsh7th/cmp-nvim-lsp'},     -- Required
-			{'hrsh7th/cmp-buffer'},       -- Optional
-			{'hrsh7th/cmp-path'},         -- Optional
-			{'saadparwaiz1/cmp_luasnip'}, -- Optional
-			{'hrsh7th/cmp-nvim-lua'},     -- Optional
-			-- Snippets
-			{'L3MON4D3/LuaSnip'},             -- Required
-			{'rafamadriz/friendly-snippets'}, -- Optional
-		}
-	}
-	-- Theme
-	use 'IamHARSHDABAS/SweetifyNeoVim'
+  -- LSP zero
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {'williamboman/mason.nvim'},           -- Optional
+      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},         -- Required
+      {'hrsh7th/cmp-nvim-lsp'},     -- Required
+      {'hrsh7th/cmp-buffer'},       -- Optional
+      {'hrsh7th/cmp-path'},         -- Optional
+      {'saadparwaiz1/cmp_luasnip'}, -- Optional
+      {'hrsh7th/cmp-nvim-lua'},     -- Optional
+      -- Snippets
+      {'L3MON4D3/LuaSnip'},             -- Required
+      {'rafamadriz/friendly-snippets'}, -- Optional
+    }
+  }
+  -- Theme
+  use 'IamHARSHDABAS/SweetifyNeoVim'
 end)
