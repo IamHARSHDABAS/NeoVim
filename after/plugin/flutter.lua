4c4f4c16b5d848729e5a96e1d0ee8c7be2db37a7
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-07 22:41:24 +0530

    Updated flutter

--- after/plugin/flutter.lua
+++ after/plugin/flutter.lua
@@ -1,1 +1,8 @@
-require("flutter-tools").setup{} -- use defaults
+require("flutter-tools").setup{
+  widget_guides = {
+    enabled = true,
+  },
+  outline = {
+    auto_open = true -- if true this will open the outline automatically when it is first populated
+  },
+}
--- lua/core/setting.lua
+++ lua/core/setting.lua
@@ -2,10 +2,10 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 vim.opt.expandtab = true
-vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.tabstop = 2
 
+vim.opt.whichwrap = "bs<>[]hl"
 vim.opt.smartindent = true
 vim.opt.wrap = false
 
@@ -13,10 +13,13 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 
 vim.opt.termguicolors = true
+vim.opt.showmode = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.incsearch = true
 vim.opt.hlsearch = false
 
 vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 8
-
 vim.opt.updatetime = 50
