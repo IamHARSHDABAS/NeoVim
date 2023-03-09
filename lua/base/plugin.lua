75684b7e03ece9f35057f84e200e7d5347807529
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-09 11:40:36 +0530

    Using packer now

--- lua/base/plugin.lua
+++ lua/base/plugin.lua
@@ -1,21 +1,20 @@
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-
--- Auto-install lazy.nvim if not present
-if not vim.loop.fs_stat(lazypath) then
-  print('Installing lazy.nvim....')
-  vim.fn.system({
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  })
-  print('Done.')
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
-vim.opt.rtp:prepend(lazypath)
+local packer_bootstrap = ensure_packer()
 
-require("lazy").setup({
-	{"IamHARSHDABAS/SweetifyNeoVim"}
-})
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'IamHARSHDABAS/SweetifyNeoVim'
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
