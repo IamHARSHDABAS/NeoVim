04732c4b4943d9ef5725ce4b3e83661df18a90c9
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-09 10:56:01 +0530

    Reworked for new base

---
+++ lua/core/config/init.lua
@@ -0,0 +1,8 @@
+require 'core.config.autopairs'
+require 'core.config.bufferline'
+require 'core.config.flutter'
+require 'core.config.indent'
+require 'core.config.lsp'
+require 'core.config.lualine'
+require 'core.config.telescope'
+require 'core.config.treesitter'
