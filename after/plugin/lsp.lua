40fa53cc4c63fd6d2e25630af87af6025aa0d621
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-11 21:45:07 +0530

    Flutter => Lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,7 +5,37 @@ local lsp = require('lsp-zero').preset({
   suggest_lsp_servers = true,
 })
 
--- (Optional) Configure lua language server for neovim
+local dart_lsp = lsp.build_options('dartls', {})
+
+require("flutter-tools").setup {
+  widget_guides = {
+    enabled = true,
+  },
+  closing_tags = {
+    highlight = "ErrorMsg",
+    prefix = "",
+    enabled = true
+  },
+  lsp = {
+    color = {
+      enabled = true,
+      background = true,
+      background_color = nil,
+      foreground = false,
+      virtual_text = true,
+      virtual_text_str = "■",
+    },
+    capabilities = dart_lsp.capabilities,
+    settings = {
+      showTodos = true,
+      completeFunctionCalls = true,
+      analysisExcludedFolders = {"<path-to-flutter-sdk-packages>"},
+      renameFilesWithClasses = "prompt",
+      enableSnippets = true,
+    }
+  }
+}
+
 lsp.nvim_workspace()
 
 lsp.setup()
