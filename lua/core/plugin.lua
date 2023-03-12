1dc577409d81ad487aeb3ba42722171bce93d869
IamHARSHDABAS <iamharshdabas@gmail.com>
2023-03-12 08:56:54 +0530

    Rm lsp-zero & cleaned

--- lua/core/plugin.lua
+++ lua/core/plugin.lua
@@ -12,47 +12,33 @@ end
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  -- Packer
+  -- Imp Stuff
+  use 'nvim-lua/plenary.nvim'
   use 'wbthomason/packer.nvim'
-  -- Lualine
-  use 'nvim-lualine/lualine.nvim'
-  -- Bufferline
+  -- UI
   use 'akinsho/bufferline.nvim'
-  -- Indent
-  use 'lukas-reineke/indent-blankline.nvim'
-  -- Autopairs
-  use 'windwp/nvim-autopairs'
-  -- Telescope
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    -- or                          , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  -- Treesitter
+  use 'nvim-lualine/lualine.nvim'
   use 'nvim-treesitter/nvim-treesitter'
+  use 'lukas-reineke/indent-blankline.nvim'
+  -- UX
+  use 'windwp/nvim-autopairs'
+  use 'nvim-telescope/telescope.nvim'
   -- Flutter
   use 'akinsho/flutter-tools.nvim'
-  -- LSP zero
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason.nvim'},           -- Optional
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},         -- Required
-      {'hrsh7th/cmp-nvim-lsp'},     -- Required
-      {'hrsh7th/cmp-buffer'},       -- Optional
-      {'hrsh7th/cmp-path'},         -- Optional
-      {'saadparwaiz1/cmp_luasnip'}, -- Optional
-      {'hrsh7th/cmp-nvim-lua'},     -- Optional
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},             -- Required
-      {'rafamadriz/friendly-snippets'}, -- Optional
-    }
-  }
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+  -- Autocompletion
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'saadparwaiz1/cmp_luasnip'
+  -- Snippets
+  use 'L3MON4D3/LuaSnip'
+  use 'rafamadriz/friendly-snippets'
   -- Theme
   use 'IamHARSHDABAS/SweetifyNeoVim'
   if packer_bootstrap then
